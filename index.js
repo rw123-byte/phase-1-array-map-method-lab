d7ff35901b518ceefbf49c9136a2cee11ec0feb0
ruthwekesa <wekesaruth569@gmail.com>
2022-08-22 12:41:03 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map((tut) => {
+        const f = tut.split(" ");
+        return f.map((i) => {
+            return i.charAt(0).toUpperCase() + i.slice(1);
+        }).join(" ");
+    })
 }
